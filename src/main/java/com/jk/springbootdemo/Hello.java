ed78c11eee7b703a4f524d9d8696cdc4b9e2b7a0
13834964701@163.com <sjh13834964701>
2018-08-14 16:26:14 +0800

    test

---
+++ src/main/java/com/jk/springbootdemo/Hello.java
@@ -0,0 +1,11 @@
+package com.jk.springbootdemo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello,this is a springboot demo";
+    }}
